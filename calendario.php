e94bc9112adf1df7f7be6dc06fc460aca9bfa5e0
Marcos Mori <gigio0107@gmail.com>
2016-03-24 15:36:14 -0300

    posição 403

--- calendario.php
+++ calendario.php
@@ -1,13 +1,37 @@
 <! capitulo 3 pag 19 !>
-<!teste de git>
+<!Lendo pelo kindle posição 403 >
+	
+<?php
+                Function linha()
+        {
+                echo "
+                        <tr>
+                                <th></th>
+                                <th></th>
+                                <th></th>
+                                <th></th>
+                                <th></th>
+                                <th></th>
+                                <th></th>
+                        </tr>
+                ";
+        }
+        ?>
+
+
 <table  border="1">
 	<tr>
 		<th>Dom</th>
-		<th>Seg</th>
-		<th>Ter</th>
-		<th>Qua</th>
-		<th>Qui</th>
-		<th>Sex</th>
-		<th>Sáb</th>	
+                <th>Seg</th>
+                <th>Ter</th>
+                <th>Qua</th>
+                <th>Qui</th>
+                <th>Sex</th>
+                <th>Sab</th>
 	</tr>
+	<?php linha(); ?>
+        <?php linha(); ?>
+        <?php linha(); ?>
+        <?php linha(); ?>
+        <?php linha(); ?>
 </table>
